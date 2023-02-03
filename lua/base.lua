810b960aefdc02b9933c696139701990e7791627
Philip George <23001002+pg328@users.noreply.github.com>
2023-02-03 16:45:56 +0000

    basic setup

---
+++ lua/base.lua
@@ -0,0 +1,40 @@
+vim.cmd('autocmd!')
+
+vim.scriptencoding = 'utf-8'
+vim.encoding = 'utf-8'
+vim.fileencoding = 'utf-8'
+
+vim.wo.number = true
+
+local o = vim.opt
+
+o.title = true
+o.autoindent = true
+o.hlsearch = true
+o.backup = false
+o.showcmd = true
+o.cmdheight = 1
+o.laststatus = 2
+o.expandtab = true
+o.scrolloff = 10
+o.shell = 'zsh'
+o.backupskip = '/tmp/*,/private/tmp/*'
+o.inccommand = 'split'
+o.ignorecase = true
+o.smarttab = true
+o.breakindent = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.ai = true -- autoindent
+o.si = true -- smartindent
+o.wrap = false -- no wrap lines
+o.backspace = 'start,eol,indent'
+o.path:append { '**' } -- recursive file search
+o.wildignore:append { '*/node_modules/*' } -- recursive file search
+o.formatoptions:append { 'r' }
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = 'set nopaste',
+})
+
